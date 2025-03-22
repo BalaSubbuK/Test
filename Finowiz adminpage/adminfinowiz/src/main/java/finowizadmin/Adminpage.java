233f4e73e6261da143e147989435c72dca0e8cf4
Balasubbu <wealwintest@gmail.com>
2025-03-22 14:47:58 +0530

    finowizscript1

---
+++ Finowiz adminpage/adminfinowiz/src/main/java/finowizadmin/Adminpage.java
@@ -0,0 +1,17 @@
+package finowizadmin;
+
+public class Adminpage {
+
+
+
+
+
+	System.setProperty("webdriver.chrome.driver",Main_class.Driver_Location());
+											WebDriver driver = new ChromeDriver();
+											System.out.println("     Testing Started     ");
+
+
+
+
+
+}
